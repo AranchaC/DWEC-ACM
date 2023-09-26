9730d7bc77e049872fb443121ee8b5f28f622cb1
AranchaC <aranchachicharro@gmail.com>
2023-09-26 12:07:56 +0200

    clase 26sep 12.07 FIN JUEGO

--- EJS VARIOS TEMA2/adivinaNum.js
+++ EJS VARIOS TEMA2/adivinaNum.js
@@ -1,25 +1,39 @@
 const MAX = 100;
 
 let getRandomInt = (max) => Math.floor(Math.random() * max)
+const partidas = []
 
-let aleat = getRandomInt(MAX)
-let num = parseInt(prompt("¡ VAMOS A JUGAR ! \n Adivina el número."))
-let contador = 1
+while(true){
+    let aleat = getRandomInt(MAX)
+    console.log(aleat)
+    let num = parseInt(prompt("¡ VAMOS A JUGAR ! \n Adivina el número."))
+    let contador = 1
 
-while (true) {  
     if (isNaN(num) === true){
         console.log("Has cancelado. \n SE ACABÓ EL JUEGO.")
         break;
     }
-    if ( num === aleat){
-        console.log(`¡¡HAS GANADO!! \n Lo has adivinado al ${contador} intento!!`)
-        break;
-    } else if (num > aleat){
-        console.log(`Tu número ${num} es mayor. \n ¡Inténtalo de nuevo!`)
-        contador++
-    } else{
-        console.log(`Tu número ${num} es menor. \n ¡Inténtalo de nuevo!`)
-        contador++
-    }
-    num = parseInt(prompt("Dime otro número."))
-} 
+
+    while (true) {  
+
+        if ( num === aleat){
+            console.log(`¡¡HAS GANADO!! \n Lo has adivinado al ${contador} intento!!`)
+            partidas.push({NºAcertado: aleat, Intentos: contador})
+            //console.log(partidas)
+            break;
+        } else if (num > aleat){
+            console.log(`Te pasaste, tu número ${num} es mayor. \n ¡Inténtalo de nuevo!`)
+            contador++
+        } else{
+            console.log(`No has llegado, tu número ${num} es menor. \n ¡Inténtalo de nuevo!`)
+            contador++
+        }
+        num = parseInt(prompt("Dime otro número."))
+
+    } //while true 2
+    //console.log(partidas)
+    console.table(partidas)
+}//while true 1
+
+
+
