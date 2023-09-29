92712a8bfa1f75d489280470efa45fcbd149a896
AranchaC <aranchachicharro@gmail.com>
2023-09-29 10:16:28 +0200

    clase 29sep 10.16

---
+++ TEMA 3/ejTema3.js
@@ -0,0 +1,61 @@
+// ej 2.1 //
+function ej1(){
+
+    let saludar = () => alert("Hola, te estoy observando...")
+    let idTiempo = setTimeout(saludar, 7000)
+}
+
+// ej 2.2 //
+function ej2(){
+    let valorInicial=10
+    let decrementar = () => {
+        valorInicial -= 1
+        if (valorInicial>0){
+            console.log(`Valor actual: ${valorInicial}`)
+        } else {
+            console.log("Tu tiempo ha terminado.")
+            clearTimeout(idTiempo)
+        }
+    }
+    let idTiempo = setInterval(decrementar, 500)
+}
+
+// ej 2.3 //
+function ej3(){
+    let fecha
+    let fechaForm
+    let mostrarHora = () =>{
+        fecha= new Date()
+        fechaForm = `La hora es ${fecha.getHours()}:${fecha.getMinutes()}:${fecha.getSeconds()}`
+
+        console.log(fechaForm)
+        document.title=fechaForm
+    }
+    let idTiempo = setInterval(mostrarHora, 5000)
+}
+
+// ej 2.4 //
+function ej4(){
+    let mostrarFecha = () =>{
+        let fechaActual = new Date()
+        console.log(fechaActual)
+    }
+
+    let idTiempo = setInterval(mostrarFecha, 2000)
+}
+
+
+// ej 2.5 //
+function ej5(){
+    let fechaFinal = new Date('2024/06/30')
+    let fechaActual = new Date()
+
+    let diasRest = () => {
+        let resta = fechaFinal - fechaActual
+        let diasRest = Math.floor(resta / (1000 * 60 * 60 * 24))
+        console.log(diasRest)
+
+    }
+    console.log(diasRest())
+    // let idTiempo = setInterval(diasRest, 2000)
+}
---
+++ TEMA 3/windows.js
@@ -0,0 +1,6 @@
+console.log("Conecta")
+
+let saludar = () => alert("hola")
+let idTiempo = setTimeout(saludar, 5000)
+
+// let idTiempo = setInterval(saludar, 5000)
