3b0458b7f62450b758bd83cf0d71354e1f9a5d22
AranchaC <aranchachicharro@gmail.com>
2023-10-03 10:14:45 +0200

    clase 03oct 10.14

--- TEMA 3/ejTema3.js
+++ TEMA 3/ejTema3.js
@@ -78,11 +78,60 @@ function ej6(){
     console.log(`Está aran?:  ${buscar("aran")}`)
 
     let borrarSocio = function(){
-        socios.pop()
         console.log(socios)
+        socios.pop()
         if(socios.length === 0){
-            clearInterval
+            clearInterval(idInterval)
         }
     }
+    idInterval=setInterval(borrarSocio,1000)
+
+    //función forEach
+    socios.forEach(function(nombre){
+        console.log(nombre);
+    })
+
+    let posicion=0
+    socios.forEach(elemento => {
+        console.log(`${posicion} ${elemento}`)
+        posicion+=1
+    })
+
+    // función map
+    let nuevoSocios = socios.map(elemento => elemento.toUpperCase())
+    console.log(nuevoSocios)
+
+    //función every
+    let sociosMas4 = socios.every(elemento => elemento.length > 3)
+    console.log(sociosMas4)
+
+    //función some
+    socios.some(elemento => elemento[0] === a)
+
+}
+
+// ej 4.2 //
+function ej7() {
+    let size = parseInt(prompt("¿Cuántos alumnos hay?"))
+    let alumnos = []
+
+    while (alumnos.length !== size){
+        let nombre = prompt("Dime nombre y nota (separado por coma)")
+        alumnos.push(`${nombre},${nota}`)
+    }
+
+    //console.log(alumnos)
+    alumnos.forEach(elemento => console.log(elemento))
+    
+    let suma = 0
+    alumnos.forEach(elemento => {
+       let info=elemento.split(",")
+        suma+=info[1]
+    })
+    
+    let media = suma / size
+    console.log(`La media es ${media}`)
+
+
 
 }
