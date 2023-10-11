94ff518f63c6381120b337db41b15555db1beee1
AranchaC <aranchachicharro@gmail.com>
2023-10-11 12:37:48 +0200

    clase 10oct 12.37

--- TEMA 3/ejTema3_set.js
+++ TEMA 3/ejTema3_set.js
@@ -1,39 +1,42 @@
-/* **** 5 MAPA *** */
+/* **** 6 SET *** */
 
-let alumnos = new Map();
+let alumnos = new Set;
 function insertar(){
     let size = parseInt(prompt("¿Cuántos alumnos quieres insertar?"))
     for (let i=0; i<size; i++) {
         let nom = prompt("Dime un nombre");
         let ed = parseInt(prompt(`Dime la edad de ${nom}`))
+        let alumno = {nombre: nom, edad: ed};
         if (alumnos.has(nom)){
             alert("Alumno ya existe.")
         } else{
-            alumnos.set(nom,ed);
+            alumnos.add(alumno);
         }      
     }//for
     console.log(alumnos)
 }
 
 function info(){
-    alumnos.forEach((ed,nom) => {
-        console.log(`El alumno ${nom} tiene ${ed} años`)
+    alumnos.forEach((alumno) => {
+        console.log(`El alumno ${alumno.nombre} tiene ${alumno.edad} años`)
     })
 }
 
 function media(){
     totalEdad = 0;
-    alumnos.forEach((ed) => totalEdad+=ed);
+    alumnos.forEach((alumno) => totalEdad+=alumno.edad);
     console.log(`La media de edad es: ${totalEdad/alumnos.size}`);
 }
 
 function buscar (){
     let nombre = prompt("¿Qué alumno quieres buscar (nombre)?");
-    if (alumnos.has(nombre)){
-        console.log(`SI Existe el alumno ${nombre}`);
-    } else {
-        console.log(`NO Existe el alumno ${nombre}`);
-    }
+        if (alumnos.nombre == nombre){
+            console.log(`SI Existe el alumno ${alumno.nombre}`);
+        } else {
+            console.log(`NO Existe el alumno ${nombre}`);
+        }
+    ;
+
 }
 
 function borrar (){
