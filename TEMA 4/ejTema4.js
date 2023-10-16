913ba6c921d32111164bf8d23f09d912fe87546e
AranchaC <aranchachicharro@gmail.com>
2023-10-16 13:30:06 +0200

    clase 16oct

---
+++ TEMA 4/ejTema4.js
@@ -0,0 +1,14 @@
+// ej 1 //
+function ej1(){
+
+    tvSamnsung = {
+        nombre: "TV Samnsung 42",
+        caegoria: "Televisores",
+        unidades: 4,
+        precio: 345.95,
+        importe: function(){
+            return (this.unidades*this.precio);
+        },
+    };
+}
+
