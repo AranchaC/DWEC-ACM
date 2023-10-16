57985e7db2b77764efbff564ba95c677a91b9681
AranchaC <aranchachicharro@gmail.com>
2023-10-16 20:26:30 +0200

    casa 16oct 20.26

--- TEMA 4/ejTema4.js
+++ TEMA 4/ejTema4.js
@@ -3,12 +3,42 @@ function ej1(){
 
     tvSamnsung = {
         nombre: "TV Samnsung 42",
-        caegoria: "Televisores",
+        categoria: "Televisores",
         unidades: 4,
         precio: 345.95,
         importe: function(){
             return (this.unidades*this.precio);
         },
     };
+}
+
+// get y set
+class Productos {
+    constructor (nombre, categoria, unidades, precio ){
+        this._nombre = nombre;
+        this._categoria = categoria;
+        this._unidades = unidades;
+        this._precio = precio;
+    }//const
+
+    get nombre(){return this._nombre;}
+    get categoria(){return this._categoria;}
+    get unidades(){return this._unidades;}
+    get precio(){return this._precio;}
+
+    set nombre(valor){this._nombre = valor;}
+    set categoria(valor){this._categoria = valor;}
+    set unidades(valor){this._unidades = valor;}
+    set precio(valor){this._precio = valor;}
+
+    importe(){
+        return this.unidades*this.precio;
+    }
+
+    getInfo(){
+        console.log(this.nombre + "(" + this.categoria + "): " + this.unidades+"uds x "+
+            this.precio+"€ = " + this.importe() + "€."
+        );
+    }
 }
 
