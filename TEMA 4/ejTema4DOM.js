c27ced4ce343b2538c9caefd674275e4e91fa869
AranchaC <aranchachicharro@gmail.com>
2023-10-20 12:36:10 +0200

    clase 20oct 12.36

--- TEMA 4/ejTema4DOM.js
+++ TEMA 4/ejTema4DOM.js
@@ -4,3 +4,14 @@ let tam = document.getElementsByClassName("tam");
 if (color){
     document.getElementsByName("titulo") ;
 }
+
+/////////////////////////////
+/// 2.2
+
+console.log("Input2 con elementById: " + 
+    document.getElementById("input2")
+)
+console.log("Input2 con elementById: " + 
+    document.getElementById("input2")
+)
+
