541b0a757e1eaf1c1c6180729d6ee705781057db
AranchaC <aranchachicharro@gmail.com>
2023-11-13 13:33:05 +0100

    clase 13nov 13.33

--- TEMA 5/Enunciado/eventos-ini.js
+++ TEMA 5/Enunciado/eventos-ini.js
@@ -7,9 +7,41 @@ var color_css = 'background: #222; color: #bada55'
 
 
 function mostrarInformacion(e){
-    
     console.clear()
-    console.log(`Usando this : %c${this.value}`,color_css);
+    //Input nombre
+    let nombre = document.getElementById("nombre").value;
+    console.log(`Nombre: %c${nombre}`,color_css);
+
+    //Input contraseña
+    let pass = document.getElementById("contraseña").value;
+    console.log(`Contraseña: %c${pass}`,color_css)
+
+    //input edad
+    let edad = document.getElementById("edad").value;
+    console.log(`Edad: %c${edad}`,color_css)
+
+    //input color
+    let color = document.getElementById("color").value;
+    console.log(`Color: %c${color}`,color_css)
+
+    //input radio género
+    let genero = document.querySelector('input[name="genero"]:checked').value;
+    console.log(`Género: %c${genero}`, color_css);
+
+    //input checkbox Idioma
+  
+    let idiomas = document.querySelectorAll('input[name="idioma"]:checked');
+    idiomas.forEach(element => {
+        console.log(`Idioma: %c${element.value}`, color_css);
+      }); 
+    
+    //input select pais
+        
+
+
+
+    // console.clear()
+    // console.log(`Usando this : %c${this.value}`,color_css);
     //console.log(`Usando Event : %c${e.target.value}`,color_css);
     
     
