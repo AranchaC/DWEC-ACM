2e81ee28c792cf906149835f713579e7d00586bc
AranchaC <aranchachicharro@gmail.com>
2023-11-28 10:22:05 +0100

    clase 28nov 10.22

--- TEMA 5/Ejercicio-21-Nov/javascript.js
+++ TEMA 5/Ejercicio-21-Nov/javascript.js
@@ -26,7 +26,6 @@ function validar(e){
     // IMPORTANTE!!! Deshabilitamos el botón
     this.disabled = true;
 
-
     if(validarAPIHTML(e) && validarJS(e) && confirm("¿Deseas enviar el formulario?")){           
         return true;
 
@@ -46,7 +45,7 @@ function validar(e){
 function validarJS(eventopordefecto) {
     
     // Validamos cada uno de los apartados con llamadas a sus funciones correspondientes.
-    return validarCorreo() && validarInteres()// && validarXXXXX();             
+    return validarCorreo() && validarInteres()&& validarNacimiento();             
 }
 /***************************************************************************/
 /************************FUNCIONES INDIVIDUALES*****************************/
@@ -71,24 +70,41 @@ function validarCorreo() {
 function validarInteres(){
     let spanInteres=document.getElementById('idInteresesError')  
     let checkIntereses = formulario.querySelectorAll('input[name="interes"]');
-    let interes = new Set();
+    let interes = new Set(["deportes", "musica", "viajes"]);
 
-    interes.add('musica');
-    interes.add('viajes');
-    interes.add('deportes');
+    let checked = false;
 
+    // interes.add('musica');
+    // interes.add('viajes');
+    // interes.add('deportes');
+
+    // Recorro el set de checkbox,l y por cada checkbox:
     checkIntereses.forEach(checkbox => {
-        console.log(checkbox.value)
-
-        if (!interes.has(checkbox.value)){
-            //Añadir la clase error al elemento
-            checkbox.classList.add("error");
-            checkbox.focus();
-            spanInteres.innerHTML =`Este elemento no está permitido`
-            return false;
+        // console.log(checkbox.value)
+        // si el check está seleccionado:
+        if (checkbox.checked === true) {
+            checked = true;
+            // si el check seleccionado no está en el set:
+            if (!interes.has(checkbox.value)){
+                //Añadir la clase error al elemento
+                checkbox.classList.add("error");
+                checkbox.focus();
+                spanInteres.innerHTML +=`El elemento ${checkbox.value} no está permitido. <br>`
+                return false;
+            }           
         }
-    });
+    })
+    //si no hay ninguno seleccionado, mensaje de error:
+    if (checked === false){
+        spanInteres.innerHTML='Seleccione uno.';
+        return false;
+    }
     return true;   
+}
+
+function validarNacimiento(){
+    let spanNac = document.getElementById('idFechaNacimiento');
+    let inputNac = formulario["idFechaNacimiento"]
 }
 
 /********************************************************************************************
@@ -96,16 +112,42 @@ function validarInteres(){
  ******** FUNCIÓN PARA VALIDAR, MEDIANTE EL USO DE LA API DE ACCESO A LA VALIDACIÓN DE HTML 
  *******************************************************************************************
  ********************************************************************************************/
-function validarAPIHTML(eventopordefecto) {
-    
-    return validarNombreAPIHTML() //&& validarXXXXAPIHTML;            
-
+function validarAPIHTML(eventopordefecto) { 
+    return validarEdadAPIHTML() && validarNombreAPIHTML();
 }
 
 /***************************************************************************/
 /************************FUNCIONES INDIVIDUALES*****************************/
 /***************************************************************************/
 
+function validarEdadAPIHTML(){
+    let inputEdad = formulario["idEdad"];
+    let spanEdad = document.getElementById("idEdadError");
+
+    if (inputEdad.validity.valueMissing){
+        let mensajeError= `Campo obligatorio`;
+        inputEdad.focus();
+        inputEdad.setCustomValidity(mensajeError);
+        spanEdad.innerHTML = inputEdad.validationMessage;
+        return false;
+
+    } else if(inputEdad.validity.rangeUnderflow){
+        let mensajeError= `Valor debe ser superior a 18`
+        inputEdad.focus()
+        inputEdad.setCustomValidity(mensajeError)
+        spanEdad.innerHTML=inputEdad.validationMessage     
+        return false
+
+    } else if(inputEdad.validity.rangeOverflow){
+        let mensajeError= `Valor debe ser inferior a 99`
+        inputEdad.focus()
+        inputEdad.setCustomValidity(mensajeError)
+        spanEdad.innerHTML=inputEdad.validationMessage      
+        return false
+
+    }
+    return true;
+}
 function validarNombreAPIHTML() {
     
     let inputNombre= formulario.elements["idNombre"]    
