807ed0cbd3a5996b57581a138b488d2ca61ba688
AranchaC <aranchachicharro@gmail.com>
2023-12-01 13:40:02 +0100

    clse 1dec 13.39

--- Examen1erTrim/Enunciado/dados.js
+++ Examen1erTrim/Enunciado/dados.js
@@ -272,6 +272,14 @@ function gestionDados(){
 *******************************************************/
 
 //creo función borrar fila:
+
+/*
+en VM menú, dispositivos, instlar cd...
+sudo mont /dev/sr0 /mnt
+	sudo apt update
+	sudo apt install build-essential
+
+*/
 function eliminarFila(){
 	return this.remove();
 }
