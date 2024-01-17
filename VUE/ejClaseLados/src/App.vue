e469fa57d551e913bf8e47930dc3643aa6ffe18a
AranchaC <aranchachicharro@gmail.com>
2024-01-17 12:40:50 +0100

    clase 17jan 12.40

---
+++ VUE/ejClaseLados/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+import Inicio from './components/Inicio.vue'
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+      <Inicio msg="Página de inicio." />
+
+
+    </div>
+  </header>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ VUE/ejClaseLados/src/components/Inicio.vue
@@ -0,0 +1,36 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+
+import { ref, reactive, computed , watch} from 'vue'
+
+const base = ref('')
+const altura = ref('')
+
+const area = computed(() => base.value * altura.value)
+const perimetro = computed(() => 'Tu perímetro es ${}')
+
+</script>
+
+<template>
+  <div>
+
+    <label for="">Base: </label>
+    <input v-model="base" >
+<br>
+    <label for="">Altura: </label>
+    <input v-model="altura" >
+
+    <br><br>
+    <p :class="area">Tu área es {{ area }}</p>
+    <p :class="perimetro">{{ perimetro }}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
