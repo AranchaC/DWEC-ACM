b08235f8ed325bb82445bc0f9b4a6b5af59c55f8
AranchaC <aranchachicharro@gmail.com>
2024-01-19 10:22:39 +0100

    clase 19jan 10.22

--- VUE/ejCuadrado/src/components/Inicio.vue
+++ VUE/ejCuadrado/src/components/Inicio.vue
@@ -14,16 +14,26 @@ const altura = ref('')
 const area = computed(() => base.value * altura.value)
 const perimetro = computed(() => 'Tu perímetro es ${}')
 
+function limpiar() {
+  largo.value = 0;
+  alto.value = 0;
+  ancho.value = 0;
+}
+
 </script>
 
 <template>
   <div>
+
+    <h2>Cuadrado:</h2>
 
     <label for="">Base: </label>
     <input v-model="base" >
 <br>
     <label for="">Altura: </label>
     <input v-model="altura" >
+<br><br>
+    <button @click="limpiar">Limpiar datos</button>  
 
     <br><br>
     <p :class="area">Tu área es {{ area }}</p>
---
+++ VUE/ejCubo/src/components/Inicio.vue
@@ -0,0 +1,97 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+
+import { ref, reactive, computed , watch} from 'vue'
+
+let largo = ref(0)
+let alto = ref(0)
+let ancho = ref(0)
+
+const area = computed(() => ancho.value * largo.value)
+const perimetro = computed(() => 2*ancho.value + 2*alto.value)
+const volumen = computed(() => ancho.value * alto.value * largo.value)
+
+const mensajes = reactive({
+  largo:"",
+  alto:"",
+  ancho:""
+})
+
+watch(largo,(nuevoValor, viejoValor) => {
+  if (isNaN(nuevoValor) || nuevoValor < 0){
+    largo.value = viejoValor;
+    mensajes.largo = `El valor no es válido.`
+  } 
+  console.log(`Valor cambiado de ${viejoValor} a ${nuevoValor}`)
+})
+
+watch(alto,(nuevoValor, viejoValor) => {
+  if (isNaN(nuevoValor) || nuevoValor < 0){
+    alto.value = viejoValor;
+    mensajes.alto = `El valor no es válido.`
+  } 
+  console.log(`Valor cambiado de ${viejoValor} a ${nuevoValor}`)
+})
+
+watch(ancho,(nuevoValor, viejoValor) => {
+  if (isNaN(nuevoValor) || nuevoValor < 0){
+    ancho.value = viejoValor;
+    mensajes.ancho = `El valor no es válido.`
+  } 
+  console.log(`Valor cambiado de ${viejoValor} a ${nuevoValor}`)
+})
+function limpiar() {
+  largo.value = 0;
+  alto.value = 0;
+  ancho.value = 0;
+}
+
+
+
+</script>
+
+<template>
+  <div>
+
+    <h2>Cubo:</h2>
+    <label for="">Largo: </label>
+    <input v-model="largo" >
+    <span class="rojo">{{ mensajes.largo }}</span>
+    <br>
+
+    <label for="">Alto: </label>
+    <input v-model="alto" >
+    <span class="rojo">{{ mensajes.alto }}</span>
+    <br>
+
+    <label for="">Ancho: </label>
+    <input v-model="ancho" >
+    <span class="rojo">{{ mensajes.ancho }}</span>
+    <br>
+
+<br>
+    <button @click="limpiar">Limpiar datos</button>  
+
+    <br><br>
+    <p :class="area">Tu área es {{ area }}</p>
+    <p :class="perimetro">Tu perímetro es {{ perimetro }}</p>
+    <p :class="volumen">Tu volumen es {{ volumen }}</p>
+
+  </div>
+</template>
+
+<style scoped>
+
+input{
+  display: block;
+}
+
+.rojo{
+  color: red;
+}
+</style>
